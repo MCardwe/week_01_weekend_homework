b1c6af80af6e0b25b25ce00271b143f40a1246a4
Max Cardwell <maximillian.cardwell@gmail.com>
2022-02-25 19:09:37 +0000

    only one test not passing

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,5 +1,8 @@
 # WRITE YOUR FUNCTIONS HERE
 
+from gettext import find
+
+
 def get_pet_shop_name(list):
     return list["name"]
 
@@ -32,10 +35,26 @@ def get_pets_by_breed(list, breed):
     return breed_amount
 
 def find_pet_by_name(list, name):
-
     for pet in list["pets"]:
         if pet["name"] == name:
             return pet
+    
+
+# def find_pet_by_name(list, name):
+    # found_pet = []
+    # for pet in list["pets"]:
+    #     if pet["name"] == name:
+    #         found_pet.append(pet)
+    #         return found_pet
+    # found_pet.append({
+    #     "name" : "Not Found",
+    #     "pet_type" : "invalid",
+    #     "breed" : "invalid",
+    #     "price" : 0
+    # }
+    # )
+
+
 
 def remove_pet_by_name(list, name):
 
@@ -62,3 +81,44 @@ def get_customer_pet_count(customer):
 
 def add_pet_to_customer(customer, pet):
     customer["pets"].append(pet)
+
+def customer_can_afford_pet(customer, pet):
+    customers_cash = customer["cash"]
+    pet_value = pet["price"]
+
+    if customers_cash >= pet_value:
+        return True
+    else:
+        return False
+
+def add_to_pets_sold(list):
+    list["admin"]["pets_sold"] += 1
+
+def add_cash_to_shop(list, cash_added):
+    list["admin"]["total_cash"] += cash_added
+
+# Dont know how to link this to final failing test
+def does_pet_exist_check(list,pet_name):
+    exists = False
+    for pet in list["pets"]:
+        if pet["name"] == pet_name:
+            exists = True
+    return exists
+
+
+    
+def sell_pet_to_customer(list, chosen_pet, customer):
+
+
+    if customer_can_afford_pet(customer, chosen_pet) == True:
+        remove_customer_cash(customer, chosen_pet["price"])
+            
+        
+        add_to_pets_sold(list)
+        add_pet_to_customer(customer, chosen_pet)
+        add_cash_to_shop(list, chosen_pet["price"])
+
+        for idx, pet in enumerate(list["pets"]):
+            if pet == chosen_pet:
+                list["pets"].pop(idx)
+    
